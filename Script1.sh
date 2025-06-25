b38fa8859bc2431153d26069465e6529cf671514
yash shinde <60213351+Yash2471999@users.noreply.github.com>
2025-06-25 14:52:08 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #This is my 1st shell script to print output
+echo "Updated File By Me"
 echo "Hello All, Welcome to AWS/Devops Class"
 echo "Hello, How are you?"
 echo "Welcome to Young Minds"
